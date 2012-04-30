ad446c68440f263cfb95a023e86076a74178688e
pima <pima@3eclipses.com>
2012-04-30 09:14:07 +0200

    refactored blender2.61 installation script

--- 00scripts/lib/0x1/install/blender.rb
+++ 00scripts/lib/0x1/install/blender.rb
@@ -1,50 +1,128 @@
 # encoding: utf-8
 # tested with ruby 1.9.3
 
-module X module Install
-require_relative '../0x1_lib.helper.rb'
+module X module Users
+  require_relative '../0x1_lib.helper.rb'
 
-class Blender
+  class BlenderInstall
 
-########################### >>>> 0x1_lib
-########################### <<<< 0x1_lib
+    def initialize(a_argv)
+      x__load_modules([:standard, :online])
+      blender_install_data_load()
+      arguments_process(a_argv)
+      install_process(@version, @platform)
+    end
 
-def initialize
-  x__load_modules([:standard, :online])
-end
+    private
 
-def install_261_linux_x86_64
+    def arguments_process(a_args)
+      abort usage() if a_args.join == 'help'
+      case a_args.size
+      when 0
+        versions = list_versions()
+        @version = x__select_item(versions, "blender version? ")
+        platforms = list_platforms(@version)
+        @platform = x__select_item(platforms, "platform? ")
+      when 2
+        arg1 = ARGV[0].to_sym
+        arg2 = ARGV[1].to_sym
+        if list_versions().include?(arg1)
+          @version = arg1
+        else
+          abort "E: arg1 (#{arg1}) is not a valid version "+
+            "(#{list_versions().join(' ')})"
+        end
+        available_platforms = list_platforms(@version)
+        if available_platforms.include?(arg2)
+          @platform = arg2
+        else
+          abort "E: arg2 (#{arg2}) is not a valid platform for version "+
+            "#{@version} (#{available_platforms.join(' ')})"
+        end
+      else
+        abort "E: please provide 2 arguments (version / platform)"
+      end
+    end
 
-  #uri_raw = "http://ip.helianova.com/index.shtml"
-  uri_raw = "http://download.blender.org/release/Blender2.61/blender-2.61-linux-glibc27-x86_64.tar.bz2"
-  
-  uri = x__parse_uri(uri_raw)
-  abort "ERROR verifying dowloaded blender_261_linux_x86_64 digest!" unless download_content = x__http_download(uri)
-  puts "downloading blender_261_linux_x86_64: done!"
+    def blender_install_data_load()
+      data_path_rel = '../../../data/0x1/install/blender_install.json'
+      data_path_abs = File.join(File.dirname(__FILE__), data_path_rel)
+      @blender_install_data = x__json_read(data_path_abs, true)
+    end
 
-  mu_softs_installed_path = "#{ENV['HOME']}/.0x1/00mu/00sourcing/0x1_softs_installed"
-  # TODO: simplify download with x__http_download_and_save method
-  soft_downloaded_dir_path = "#{mu_softs_installed_path}/blender/2.61/linux_x86_64/00install"
-  abort "ERROR: #{soft_downloaded_dir_path} directory exists already" if x__is_a_dir?(soft_downloaded_dir_path)
-  abort "ERROR: can't create directory #{soft_downloaded_dir_path}" unless x__mkdir_p(soft_downloaded_dir_path)
-  soft_downloaded_path = "#{soft_downloaded_dir_path}/blender-2.61-linux-glibc27-x86_64.tar.bz2"
-  abort "ERROR: can't write file #{soft_downloaded_path}" unless x__file_write(download_content, soft_downloaded_path)
-  abort "ERROR: can't read downloaded file #{soft_downloaded_path}" unless check_digest = x__file_read(soft_downloaded_path)
-  abort "ERROR verifying dowloaded blender_261_linux_x86_64 digest!" unless x__digest_create(check_digest) == '6b42d76da83a07e64695920e1163f70264ed67ee156f79b0786c97f927fde0e4'
-  puts "verified dowloaded blender_261_linux_x86_64 digest: done !"
-  soft_extract_dir_path = "#{mu_softs_installed_path}/blender/2.61/linux_x86_64/00install/blender-2.61-linux-glibc27-x86_64"
-  abort "ERROR: extract path #{soft_extract_dir_path} exists already" if x__is_a_dir?(soft_extract_dir_path)
-  abort "ERROR: can't extract #{soft_downloaded_path} on #{soft_installed_dir_path}" unless system "tar jxvf #{soft_downloaded_path} -C #{soft_downloaded_dir_path}"
-  soft_installed_dir_path = "#{mu_softs_installed_path}/blender/2.61/linux_x86_64/00installed"
-  abort "ERROR: #{soft_installed_dir_path} directory exists already" if x__is_a_dir?(soft_installed_dir_path)
-  abort "ERROR: can't move #{soft_extract_dir_path} to #{soft_installed_dir_path}" unless x__dir_move(soft_extract_dir_path, soft_installed_dir_path)
-  soft_executable_path = "#{soft_installed_dir_path}/blender"
-  soft_executable_symlink = "#{mu_softs_installed_path}/00bin/blender2.61_linux_x86_64"
-  abort "ERROR: can't create symlink #{soft_executable_symlink} pointing to #{soft_executable_path}" unless x__symlink_create(soft_executable_path, soft_executable_symlink)
+    def usage()
+      usage_path_rel = '../../../data/0x1/install/blender_usage'
+      usage_path_abs = File.join(File.dirname(__FILE__), usage_path_rel)
+      x__file_read(usage_path_abs)
+    end
 
-end
+    def list_versions()
+      @blender_install_data.keys
+    end
 
-end
+    def list_platforms(l_version)
+      @blender_install_data[l_version].keys
+    end
+
+    def install_process(l_version, l_platform)
+      uri_raw = @blender_install_data[@version][@platform][0]
+      digest = @blender_install_data[@version][@platform][1]
+      mu_softs_installed_path = "#{Dir.home}/.0x1/00mu/"+
+        "00sourcing/0x1_softs_installed"
+      soft_downloaded_dir_path = "#{mu_softs_installed_path}/blender/"+
+        "#{@version}/#{@platform}/00install"
+      if x__is_a_dir?(soft_downloaded_dir_path)
+        abort "ERROR: #{soft_downloaded_dir_path} directory exists already"
+      end
+
+      puts "I: starting installation: blender #{@version} for #{@platform} ..."
+
+      uri = x__parse_uri(uri_raw)
+      puts "I: downloading blender_261_linux_x86_64..."
+      unless downloaded_content = x__http_download(uri)
+        abort "ERROR verifying dowloaded blender_261_linux_x86_64 digest!"
+      end
+
+      unless x__mkdir_p(soft_downloaded_dir_path)
+        abort "ERROR: can't create directory #{soft_downloaded_dir_path}"
+      end
+      soft_downloaded_path = "#{soft_downloaded_dir_path}/blender-2.61-linux-glibc27-x86_64.tar.bz2"
+      unless x__file_write(downloaded_content, soft_downloaded_path)
+        abort "ERROR: can't write file #{soft_downloaded_path}"
+      end
+      unless check_digest = x__file_read(soft_downloaded_path)
+        abort "ERROR: can't read downloaded file #{soft_downloaded_path}"
+      end
+      unless x__digest_create(check_digest) == digest
+        abort "ERROR verifying dowloaded blender_261_linux_x86_64 digest!"
+      end
+      puts "verified dowloaded blender_261_linux_x86_64 digest: done !"
+      soft_extract_dir_path = "#{mu_softs_installed_path}/blender/"+
+        "#{@version}/#{@platform}/00install/blender-2.61-linux-glibc27-x86_64"
+      if x__is_a_dir?(soft_extract_dir_path)
+        abort "ERROR: extract path #{soft_extract_dir_path} exists already"
+      end
+      unless system "tar jxvf #{soft_downloaded_path} -C #{soft_downloaded_dir_path}"
+        abort "ERROR: can't extract #{soft_downloaded_path} on #{soft_installed_dir_path}"
+      end
+      soft_installed_dir_path = "#{mu_softs_installed_path}/blender/"+
+        "#{@version}/#{@platform}/00installed"
+      if x__is_a_dir?(soft_installed_dir_path)
+        abort "ERROR: #{soft_installed_dir_path} directory exists already"
+      end
+      unless x__dir_move(soft_extract_dir_path, soft_installed_dir_path)
+        abort "ERROR: can't move #{soft_extract_dir_path} to #{soft_installed_dir_path}"
+      end
+      soft_executable_path = "#{soft_installed_dir_path}/blender"
+      soft_executable_symlink = "#{mu_softs_installed_path}/00bin/blender2.61_linux_x86_64"
+      unless x__symlink_create(soft_executable_path, soft_executable_symlink)
+        abort "ERROR: can't create symlink #{soft_executable_symlink} "+
+          "pointing to #{soft_executable_path}"
+      end
+
+    end
+
+  end
 
 end end
 
