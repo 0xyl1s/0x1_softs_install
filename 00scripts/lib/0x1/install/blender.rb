f9fd59965174673816f1dc06f4ddc694bd2164fe
pima <pima@3eclipses.com>
2012-05-08 06:39:58 +0200

    refactored blender install script

--- 00scripts/lib/0x1/install/blender.rb
+++ 00scripts/lib/0x1/install/blender.rb
@@ -8,7 +8,14 @@ module X module Users
 
     def initialize(a_argv)
       x__load_modules([:standard, :online])
-      blender_install_data_load()
+      @usage_path_abs = x__rel_abs_path(__FILE__,
+                                        '../../../data/0x1/install/'+
+                                        'blender_usage')
+      @data_path_abs = x__rel_abs_path(__FILE__,
+                                       '../../../data/0x1/install/'+
+                                       'blender_install.json')
+      soft_install_data_load()
+      @soft_name = 'blender'
       arguments_process(a_argv)
       install_process()
     end
@@ -20,7 +27,7 @@ module X module Users
       case a_args.size
       when 0
         versions = list_versions()
-        @version = x__select_item(versions, "blender version? ")
+        @version = x__select_item(versions, "#{@soft_name} version? ")
         platforms = list_platforms(@version)
         @platform = x__select_item(platforms, "platform? ")
       when 2
@@ -44,75 +51,70 @@ module X module Users
       end
     end
 
-    def blender_install_data_load()
-      data_path_rel = '../../../data/0x1/install/blender_install.json'
-      data_path_abs = File.join(File.dirname(__FILE__), data_path_rel)
-      @blender_install_data = x__json_read(data_path_abs, true)
+    def soft_install_data_load()
+      @soft_install_data = x__json_read(@data_path_abs, true)
     end
 
     def usage()
-      usage_path_rel = '../../../data/0x1/install/blender_usage'
-      usage_path_abs = File.join(File.dirname(__FILE__), usage_path_rel)
       x__file_read(usage_path_abs)
     end
 
     def list_versions()
-      @blender_install_data.keys
+      @soft_install_data.keys
     end
 
     def list_platforms(l_version)
-      @blender_install_data[l_version].keys
+      @soft_install_data[l_version].keys
     end
 
     def install_process()
-      uri_raw = @blender_install_data[@version][@platform][0]
+      uri_raw = @soft_install_data[@version][@platform][0]
       uri = x__parse_uri(uri_raw)
       downloaded_filename = x__filename_from_parsed_uri(uri)
       downloaded_file_extension = '.tar.bz2'
       downloaded_basename = File.basename(downloaded_filename,
                                           downloaded_file_extension)
       installed_path = "#{Dir.home}/.0x1/00mu/00sourcing/0x1_softs_installed"
-      install_path = "#{installed_path}/blender/"+
-        "#{@version}/#{@platform}/00install"
+      soft_ver_path = "#{@soft_name}/#{@version}/#{@platform}"
+      install_path = "#{installed_path}/#{@soft_name}/"+
+      "#{@version}/#{@platform}/00install"
       x__abort_if_is_a_dir(install_path, true)
+      soft_downloaded_path = "#{install_path}/#{downloaded_filename}"
+      digest_source = @soft_install_data[@version][@platform][1]
+      soft_extract_dir_path = "#{installed_path}/#{soft_ver_path}/00install/"+
+      "#{downloaded_basename}"
+      x__abort_if_is_a_dir(soft_extract_dir_path, true)
+      soft_installed_dir_path = "#{installed_path}/#{soft_ver_path}"+
+      "/00installed"
+      x__abort_if_is_a_dir(soft_installed_dir_path, true)
+      soft_executable_path = "#{soft_installed_dir_path}/#{@soft_name}"
+      soft_ver_name = "#{@soft_name}_#{@version}_#{@platform}"
+      installed_bin_path = "#{installed_path}/00bin"
+      soft_executable_symlink = "#{installed_bin_path}/#{soft_ver_name}"
 
-      puts "I: starting installation: blender #{@version} for #{@platform} ..."
+      puts "I: starting installation: #{@soft_name} #{@version} for "+
+      "#{@platform}..."
 
       x__abort_unless_mkdir_p(install_path, true)
-      soft_downloaded_path = "#{install_path}/#{downloaded_filename}"
-      downloaded_content = x__http_download(uri)
-      x__abort_unless_file_write(downloaded_content,
-                                 soft_downloaded_path, true)
 
-      downloaded_file = x__file_read(soft_downloaded_path)
-
-      digest_source = @blender_install_data[@version][@platform][1]
-      digest_checked = x__digest_create(downloaded_file)
-      x__abort_unless_digest_checked(digest_source, digest_checked, true)
-      puts "I: verified dowloaded #{downloaded_filename} digest: done !"
-
-      soft_extract_dir_path = "#{installed_path}/blender/"+
-        "#{@version}/#{@platform}/00install/#{downloaded_basename}"
-      x__abort_if_is_a_dir(soft_extract_dir_path, true)
+      x__abort_unless_download_file_check_digest(uri_raw,
+                                                 downloaded_filename,
+                                                 install_path,
+                                                 'sha256',
+                                                 digest_source,
+                                                 true)
 
       x__abort_unless_extract_tarbz2(soft_downloaded_path, install_path, true)
 
-      soft_installed_dir_path = "#{installed_path}/blender/"+
-        "#{@version}/#{@platform}/00installed"
-      x__abort_if_is_a_dir(soft_installed_dir_path, true)
       x__abort_unless_dir_move(soft_extract_dir_path,
                                soft_installed_dir_path, true)
 
-      soft_executable_path = "#{soft_installed_dir_path}/blender"
-      symlink = "blender_#{@version}_#{@platform}"
-      installed_bin_path = "#{installed_path}/00bin"
-      soft_executable_symlink = "#{installed_bin_path}/#{symlink}"
       x__abort_unless_symlink_create(soft_executable_path,
                                      soft_executable_symlink, true)
 
       x__abort_unless_chdir(installed_bin_path)
-      x__abort_unless_symlink_create(symlink, "blender", true)
-      puts "I: installed: blender #{@version} for #{@platform} ..."
+      x__abort_unless_symlink_create(soft_ver_name, @soft_name, true)
+      puts "\nI: installed: #{@soft_name} #{@version} for #{@platform} ..."
     end
   end
 
