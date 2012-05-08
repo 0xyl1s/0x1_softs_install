76b3e657a0e536ba08e73fdeca65168b2051690c
pima <pima@3eclipses.com>
2012-05-08 22:38:12 +0200

    test file rename

--- 00scripts/lib/0x1/users/blender_install.rb
+++ 00scripts/lib/0x1/users/blender_install.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 # tested with ruby 1.9.3
-# test >> ../../../tests/0x1/users/blender_install.rb
+# test >> ../../../tests/0x1/users/test_blender_install.rb
 
 module X module Users
   require_relative '../0x1_lib.helper.rb'
@@ -14,67 +14,6 @@ module X module Users
       @soft_name = 'blender'
       soft_install_data_load()
       arguments_process(a_argv)
-      run()
-    end
-
-    private
-
-    def usage()
-      usage_path_abs = x__abort_unless_rel_abs_path(__FILE__, @usage_path_rel)
-      x__file_read(usage_path_abs)
-    end
-
-    def arguments_process(a_args)
-      abort usage() if a_args.join == 'help'
-      case a_args.size
-      when 0
-        versions = list_versions()
-        @version = x__select_item(versions, "#{@soft_name} version? ")
-        platforms = list_platforms(@version)
-        @platform = x__select_item(platforms, "platform? ")
-        @overwrite_symlink = x__confirm("\nOverwrite #{@soft_name} symlink"+
-                                        " if needed (y/n)? ")
-      when 3
-        arg1 = ARGV[0].to_sym
-        if list_versions().include?(arg1)
-          @version = arg1
-        else
-          abort "E: arg1 (#{arg1}) is not a valid version "+
-            "(#{list_versions().join(' ')})"
-        end
-        arg2 = ARGV[1].to_sym
-        available_platforms = list_platforms(@version)
-        if available_platforms.include?(arg2)
-          @platform = arg2
-        else
-          abort "E: arg2 (#{arg2}) is not a valid platform for version "+
-            "#{@version} (#{available_platforms.join(' ')})"
-        end
-        arg3 = ARGV[2]
-        if arg3 == 'true'
-          @overwrite_symlink = true
-        elsif arg3 == 'false'
-          @overwrite_symlink = false
-        else
-          abort "E: overwrite_symlink must be true or false (#{arg3})."
-        end
-      else
-        abort "E: please provide 3 arguments (version / platform /"+
-          " overwrite_symlink)"
-      end
-    end
-
-    def soft_install_data_load()
-      data_path_abs = x__abort_unless_rel_abs_path(__FILE__, @data_path_rel)
-      @soft_install_data = x__json_read(data_path_abs, true)
-    end
-
-    def list_versions()
-      @soft_install_data.keys
-    end
-
-    def list_platforms(l_version)
-      @soft_install_data[l_version].keys
     end
 
     def run()
@@ -135,6 +74,66 @@ module X module Users
 
       puts "\nI: installed: #{@soft_name} #{@version} for #{@platform} ..."
     end
+    private
+
+    def usage()
+      usage_path_abs = x__abort_unless_rel_abs_path(__FILE__, @usage_path_rel)
+      x__file_read(usage_path_abs)
+    end
+
+    def arguments_process(a_args)
+      abort usage() if a_args.join == 'help'
+      case a_args.size
+      when 0
+        versions = list_versions()
+        @version = x__select_item(versions, "#{@soft_name} version? ")
+        platforms = list_platforms(@version)
+        @platform = x__select_item(platforms, "platform? ")
+        @overwrite_symlink = x__confirm("\nOverwrite #{@soft_name} symlink"+
+                                        " if needed (y/n)? ")
+      when 3
+        arg1 = ARGV[0].to_sym
+        if list_versions().include?(arg1)
+          @version = arg1
+        else
+          abort "E: arg1 (#{arg1}) is not a valid version "+
+            "(#{list_versions().join(' ')})"
+        end
+        arg2 = ARGV[1].to_sym
+        available_platforms = list_platforms(@version)
+        if available_platforms.include?(arg2)
+          @platform = arg2
+        else
+          abort "E: arg2 (#{arg2}) is not a valid platform for version "+
+            "#{@version} (#{available_platforms.join(' ')})"
+        end
+        arg3 = ARGV[2]
+        if arg3 == 'true'
+          @overwrite_symlink = true
+        elsif arg3 == 'false'
+          @overwrite_symlink = false
+        else
+          abort "E: overwrite_symlink must be true or false (#{arg3})."
+        end
+      else
+        abort "E: please provide 3 arguments (version / platform /"+
+          " overwrite_symlink)"
+      end
+    end
+
+    def soft_install_data_load()
+      data_path_abs = x__abort_unless_rel_abs_path(__FILE__, @data_path_rel)
+      @soft_install_data = x__json_read(data_path_abs, true)
+    end
+
+    def list_versions()
+      @soft_install_data.keys
+    end
+
+    def list_platforms(l_version)
+      @soft_install_data[l_version].keys
+    end
+
   end
 
 end end
